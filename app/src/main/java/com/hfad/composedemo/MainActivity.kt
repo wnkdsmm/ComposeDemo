7210e49145f0aba0d629384561d067cea63514af
wnkdsmm <lwjfkg@mail.ru>
2025-10-06 09:00:14 +0700

    Start project

--- app/src/main/java/com/hfad/composedemo/MainActivity.kt
+++ app/src/main/java/com/hfad/composedemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android/",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
